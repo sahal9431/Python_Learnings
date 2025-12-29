9925b1bc63e791ce13c0f9fd6341b928938a4584
sahal9431 <sahalms9061@gmail.com>
2025-12-29 10:08:30 +0530

    New_commit

--- D_01/APPS/feet_meter_con.py
+++ D_01/APPS/feet_meter_con.py
@@ -1,22 +1,29 @@
 import FreeSimpleGUI as lm
 
+lm.theme('Black')
 label1 = lm.Text("Enter feet: ")
 inputbox1 = lm.Input(key='feet')
 label2 = lm.Text("Enter inches: ")
 inputbox2 = lm.Input(key='inches')
 convert_button = lm.Button('Convert')
+exit_button = lm.Button('Exit')
 result = lm.Text("", key='result') # To display the result
-layout = [[label1, inputbox1],[label2, inputbox2],[convert_button, result]]
+layout = [[label1, inputbox1],[label2, inputbox2],[convert_button, exit_button, result]]
 window = lm.Window('Converter', layout= layout, font=('Times New Roman', 20))
 while True:
     event, values = window.read()
     match event:
         case 'Convert':
-            feet = float(values['feet'])
-            inches = float(values['inches'])
-            meters = feet * 0.3048 + inches * 0.0254
-            #lm.popup(f'{feet} feet and {inches} inches is {meters} meters') # Show popup with result
-            window['result'].update(f"{meters} m")
+            try:
+                feet = float(values['feet'])
+                inches = float(values['inches'])
+                meters = feet * 0.3048 + inches * 0.0254
+                #lm.popup(f'{feet} feet and {inches} inches is {meters} meters') # Show popup with result
+                window['result'].update(f"{meters} m")
+            except ValueError:
+                lm.popup("Please enter valid numbers", font=('Times New Roman', 20))
+        case 'Exit':
+            break
         case lm.WIN_CLOSED:
             break
 window.close()
--- D_01/APPS/to_do_app.py
+++ D_01/APPS/to_do_app.py
@@ -1,27 +1,38 @@
 import FreeSimpleGUI as lm
-from D_01.to_do import functions
+from ..to_do import functions
+import time
+import os
 
+if not os.path.exists("todos.txt"): # Create the file if it doesn't exist
+    with open("todos.txt", "w") as file:
+        pass
+
+lm.theme('DarkPurple4')
 label = lm.Text("Type a TO_DO")
 input_box = lm.InputText(tooltip="Enter TODO", key="todo")
 add_button = lm.Button('Add')
 edit_button = lm.Button('Edit')
-show_button = lm.Button('Show')
 remove_button = lm.Button('Remove')
-list_box = lm.Listbox(values=functions.get_todos(), key='todos',
+exit_button = lm.Button('Exit')
+clock = lm.Text('', key='clock')
+list_box = lm.Listbox(values=functions.get_todos(), 
+                      key='todos',
                       enable_events=True, size=[45, 10])
 layout = [[label], 
+          [clock],
           [input_box, add_button], 
           [list_box, edit_button], 
-          [show_button], 
-          [remove_button]]
+          [remove_button], 
+          [exit_button]]
 window = lm.Window('My T0-Do App', 
                    layout=layout, 
                            font=('Times New Roman', 20))
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    print(values['todos'])
+    event, values = window.read(timeout=10)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    # print(event)
+    # print(values)
+    # print(values['todos'])
     match event:
         case 'Add':
             todos = functions.get_todos()
@@ -29,15 +40,32 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + '\n'
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + '\n'
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                lm.popup("Please select an item first", 
+                         font=('Times New Roman', 20))
         case 'todos': # Listbox selected
             window['todo'].update(value=values['todos'][0])
+        case 'Remove':
+            try:
+                todo_to_remove = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_remove)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='') # Clear input box
+            except IndexError:
+                lm.popup("Please select an item first", 
+                         font=('Times New Roman', 20))
+        case 'Exit':
+            break
         case lm.WIN_CLOSED:
             break
 print('Bye!')
